b67bc801109e3f18f382cd6e9eec133fd0bbe46f
ericsyoung@live.com <>
2022-07-30 06:20:31 +0900

    Color Picker mostly fixed, just need to adjust for multiple inputs

--- Assets/Scripts/ColorPicker.cs
+++ Assets/Scripts/ColorPicker.cs
@@ -10,7 +10,7 @@ using UnityEngine.EventSystems;
 [Serializable]
 public class ColorEvent : UnityEvent<Color> { }
 
-public class ColorPicker : MonoBehaviour, IPointerClickHandler
+public class ColorPicker : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
     public TextMeshProUGUI DebugText;
     public ColorEvent OnColorPreview;
@@ -21,6 +21,8 @@ public class ColorPicker : MonoBehaviour, IPointerClickHandler
     RectTransform Rect;
     Texture2D ColorTexture;
 
+    private bool tracking = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,66 +31,45 @@ public class ColorPicker : MonoBehaviour, IPointerClickHandler
         ColorTexture = GetComponent<Image>().mainTexture as Texture2D;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vector2 delta;
-
-        Vector2 mousePos = Input.mousePosition;
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(Rect, Input.mousePosition, null, out delta);
-
-            
-        string debug = "mousePosition=" + Input.mousePosition;
-        debug += "<br>delta=" + delta;
-
-        float width = Rect.rect.width;
-        float height = Rect.rect.height;
-        delta += new Vector2(width * .5f, height * .5f);
-        debug += "<br>offset delta=" + delta;
-
-        if (width <= height) { max = width; } //If we don't check the bounds of the color picker, we'll get some errors
-        else { max = height; }                //We also can't guarantee they'll stay the same size, so we'll check on Update()
-
-
-        float x = Mathf.Clamp(delta.x / width, 0f, max);
-        float y = Mathf.Clamp(delta.y / height, 0f, max);
-        debug += "<br>x=" + x + "y=" + y;
-
-        int texX = Mathf.RoundToInt(x * ColorTexture.width);
-        int texY = Mathf.RoundToInt(y * ColorTexture.height);
-        debug += "<br>texX=" + texX + " texY=" + texY;
-
-        Color color = ColorTexture.GetPixel(texX, texY);
-
-        DebugText.color = color;
-
-        DebugText.text = debug;
-        if(RectTransformUtility.RectangleContainsScreenPoint(Rect, mousePos)){
-           OnColorPreview?.Invoke(color); 
-        }
-            
-
-        if(Input.GetMouseButtonDown(0) && delta.x > 0 && delta.x < width && delta.y > 0 && delta.y < height)
-        {
-            OnColorSelect?.Invoke(color);
-        }              
-          
-
-
-    }
-
     public void OnPointerClick(PointerEventData data)
     {
-        Debug.Log("Clicked Location:" + data.position);
-        ColorChecker(data.position);
+        Color color = ColorChecker(data.position);
+
+        OnColorSelect?.Invoke(color);
     }
 
-    private void ColorChecker(Vector2 pos)
+    public void OnPointerEnter(PointerEventData data)
+    {
+        Camera camera = data.enterEventCamera;
+        Debug.Log(camera.name);
+        Color color = ColorChecker(data.position);
+
+        Debug.Log("Raycast Screen Pos: " + data.pointerCurrentRaycast.screenPosition);
+        Debug.Log("Raycast World Pos: " + data.pointerCurrentRaycast.worldPosition);
+        Debug.Log("Event Pos: " + data.position);
+
+        tracking = true;
+        StartCoroutine(PreviewCo(data));
+     
+        //OnColorPreview?.Invoke(color);
+    }
+
+    public void OnPointerExit(PointerEventData data)
+    {
+        tracking = false;
+    }
+
+    public void OnPointerMove(PointerEventData data)
+    {
+
+    }
+
+    private Color ColorChecker(Vector2 eventPos)
     {
         Vector2 delta;
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(Rect, pos, null, out delta);
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(Rect, eventPos, Camera.main, out delta);
 
-        string debug = "mousePosition=" + Input.mousePosition;
+        string debug = "Position=" + eventPos;
         debug += "<br>delta=" + delta;
 
         float width = Rect.rect.width;
@@ -114,6 +95,20 @@ public class ColorPicker : MonoBehaviour, IPointerClickHandler
 
         DebugText.text = debug;
 
-        Debug.Log("Color: " + color.ToString());
+        //Debug.Log(debug + " Color: " + color);
+
+        return color;
+
+    }
+
+    private IEnumerator PreviewCo(PointerEventData data)
+    {
+        while (tracking)
+        {
+            Debug.Log(data.pointerId);
+            Color color = ColorChecker(data.position);
+            OnColorPreview?.Invoke(color);
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 }
